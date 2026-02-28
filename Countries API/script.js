879e4e689e2a040a2e07ae7b610a5130575c026b
ajaz-mohammad <ajazmohammad2003@gmail.com>
2026-02-28 22:13:55 +0530

    Countries Icons

--- Countries API/script.js
+++ Countries API/script.js
@@ -1,19 +1,25 @@
+const countriesContainer = document.querySelector(".countries-container");
 
+fetch("https://restcountries.com/v3.1/all?fields=name,capital,population,flags")
+    .then((response) => response.json())
+    .then((data) => {
+        data.forEach((country) => {
+            
+            const countryCard = document.createElement("a");
+            countryCard.classList.add("countryCard");
+            
+            const HTML = `
+            <img src="${country.flags.svg}" alt="${country.name.common} Flag">
+            
+            <div class="card-text">
+                <h3>${country.name.common}</h3>
+                <p><b>Capital:</b> ${country.capital}</p>
+                <p><b>Population:</b> ${country.population.toLocaleString()}</p>
+            </div>
+`;
 
-const countryCard = document.createElement("a");
+countryCard.innerHTML = HTML;
 
-countryCard.classList.add("countryCard");
-
-const cardImg = document.createElement("img");
-cardImg.src = "https://flagcdn.com/w320/in.png";
-countryCard.append(cardImg);
-console.log(cardImg);
-
-
-// fetch("https://restcountries.com/v3.1/all?fields=name,flags,capital,population,region")
-//     .then((response) => response.json())
-//     .then((data) => {
-//         data.forEach((country) => {
-//             console.log(country);
-//         });
-//     })
+countriesContainer.appendChild(countryCard);
+        });
+    });
