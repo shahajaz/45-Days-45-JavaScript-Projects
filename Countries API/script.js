e0942875d6fa8c9b552e830380ea4490b6f5e4e3
ajaz-mohammad <ajazmohammad2003@gmail.com>
2026-03-01 19:54:12 +0530

    Countries Info

--- Countries API/script.js
+++ Countries API/script.js
@@ -1,12 +1,13 @@
 const countriesContainer = document.querySelector(".countries-container");
 
-fetch("https://restcountries.com/v3.1/all?fields=name,capital,population,flags")
+fetch("https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags")
     .then((response) => response.json())
     .then((data) => {
         data.forEach((country) => {
             
             const countryCard = document.createElement("a");
             countryCard.classList.add("countryCard");
+            countryCard.href = `/country.html?name=${country.name.common}`;
             
             const HTML = `
             <img src="${country.flags.svg}" alt="${country.name.common} Flag">
@@ -14,6 +15,7 @@ fetch("https://restcountries.com/v3.1/all?fields=name,capital,population,flags")
             <div class="card-text">
                 <h3>${country.name.common}</h3>
                 <p><b>Capital:</b> ${country.capital}</p>
+                <p><b>Region:</b> ${country.region}</p>
                 <p><b>Population:</b> ${country.population.toLocaleString()}</p>
             </div>
 `;
